2e3edac47d4b5773e923dcc2767be77860d6316a
Cris <cristinamihalciuc@yahoo.com>
2023-08-13 22:20:38 +0300

    See added items in the Cart page

--- src/scripts/Cart.jsx
+++ src/scripts/Cart.jsx
@@ -1,5 +1,62 @@
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import Item from "./Item";
+
 const Cart = () => {
-  return <div>Cart Page</div>;
+  const [cartItems, setCartItems] = useOutletContext();
+  const [items, setItems] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(`https://fakestoreapi.com/products`, {
+          mode: "cors",
+        });
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        let actualItems = await response.json();
+        setItems(actualItems);
+        setError(null);
+      } catch (error) {
+        setError(error.message);
+        setItems(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
+  if (error) return <p>A network error was encountered</p>;
+  if (loading) return <p>Loading...</p>;
+
+  let fullCartItems = [];
+  for (let i = 0; i < cartItems.length; i++) {
+    fullCartItems.push(items[parseInt(cartItems[i].id) - 1]);
+  }
+
+  return (
+    <div>
+      <ul>
+        {items &&
+          fullCartItems.map((item) => (
+            <Item
+              key={item.id}
+              item={item}
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+            />
+          ))}
+      </ul>
+      <div>Total: $</div>
+      <div>CheckOut</div>
+    </div>
+  );
 };
 
 export default Cart;
